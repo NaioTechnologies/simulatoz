5e0639a47fcec9c1e47d976f15f22baeaee09cff
fanny <fanny@naio-technologies.com>
2017-01-12 11:41:30 +0100

    Sans termios

--- oz440_gazebo/src/Bridge.cpp
+++ oz440_gazebo/src/Bridge.cpp
@@ -40,59 +40,6 @@ static pid_t gettid( void )
     return syscall( __NR_gettid );
 }
 
-
-// ##################################################################################################
-// ##################################################################################################
-// ##################################################################################################
-
-#include <termios.h>
-#include <stdio.h>
-
-static struct termios old_t, new_t;
-
-/* Initialize new terminal i/o settings */
-void initTermios( int echo )
-{
-    tcgetattr( 0, &old_t ); /* gab old terminal i/o settings */
-
-    new_t = old_t; /* make new settings same as old settings */
-    new_t.c_lflag &= ~ICANON; /* disable buffered i/o */
-    new_t.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
-
-    tcsetattr( 0, TCSANOW, &new_t ); /* use these new terminal i/o settings now */
-}
-
-
-/* Restore old terminal i/o settings */
-void resetTermios( void )
-{
-    tcsetattr( 0, TCSANOW, &old_t );
-}
-
-/* Read 1 character - echo defines echo mode */
-char getch_( int echo )
-{
-    char ch;
-
-    initTermios( echo );
-    ch = getchar( );
-    resetTermios( );
-
-    return ch;
-}
-
-/* Read 1 character without echo */
-char getch( void )
-{
-    return getch_( 0 );
-}
-
-/* Read 1 character with echo */
-char getche( void )
-{
-    return getch_( 1 );
-}
-
 // #####################################################################################################################
 // ---------------------------------------------------------------------------------------------------------------------
 // #####################################################################################################################
