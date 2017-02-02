8dfc8cc03f736d3a764054f440b0dbffc7584d74
fanny <fanny@naio-technologies.com>
2017-02-02 14:21:23 +0100

    new worlds

---
+++ oz440_gazebo/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fanny/catkin_ws/src/simulatoz/oz440_gazebo
+# Build directory: /home/fanny/catkin_ws/src/simulatoz/oz440_gazebo/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
