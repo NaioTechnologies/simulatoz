2407a99dea624f76d5f48520230bcc3787603b78
fanny <fanny@naio-technologies.com>
2017-02-16 09:48:59 +0100

    rebased

--- oz440_gazebo/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fanny/catkin_ws/src/simulatoz/oz440_gazebo
-# Build directory: /home/fanny/catkin_ws/src/simulatoz/oz440_gazebo/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
