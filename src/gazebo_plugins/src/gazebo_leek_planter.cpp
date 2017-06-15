1ee049c54f63d3f5f90bd9820274fb998491e039
Simon <simon@naio-technologies.com>
2017-06-15 11:49:18 +0000

    working LeekPlanter

--- src/gazebo_plugins/src/gazebo_leek_planter.cpp
+++ src/gazebo_plugins/src/gazebo_leek_planter.cpp
@@ -18,31 +18,49 @@ public:
 		sdf::ElementPtr leek = sdf->GetFirstElement();
 		if( !leek ) return;
 
+		physics::ModelPtr model = parent->GetModel( "heightmap" );
+		physics::HeightmapShape* hm = dynamic_cast<physics::HeightmapShape*>( model->GetLink("link")->GetCollision("collision")->GetShape().get() );
+		math::Vector3 size = hm->GetSize(); 
+		math::Vector2i vc = hm->GetVertexCount();
+		double ss = static_cast<double>( hm->GetSubSampling() );
+
 		do
 		{
-			if( leek->GetName() != "pose" ) continue;
+			std::string modelname;
+			double zdiff;
+			if( leek->GetName() == "leek" )
+			{
+				modelname = "Leek";
+				zdiff = 0.1;
+			}
+			else if ( leek->GetName() == "redpost" )
+			{
+				modelname = "Red_stick";
+				zdiff = 0.15;
+			}
+			else if ( leek->GetName() == "cabbage" )
+			{
+				modelname = "Cabbage";
+				zdiff = 0.1;
+			}
+			else
+			{
+				continue;
+			}
 			math::Vector2d xy = leek->Get<math::Vector2d>();	
 			double x = xy.x;
 			double y = xy.y;
-			double z = 5;
-			for( ; z >= -5; z -= 0.05 )
-			{
-				if( !parent->GetModelBelowPoint( math::Vector3( x, y, z ) ) )
-				{
-					break;
-				} 
-			}
-			
-			z += 0.1;
+			double z = hm->GetHeight( (x + size.x/2)/size.x*vc.x - 1, (y + size.y/2)/size.y*vc.y - 1);
+			z += zdiff;
 			std::ostringstream sdftext;
 			sdftext << "<sdf version='1.6'>\n"
-				<< "  <model name=\"leek_" << n << "\">"
-				<< "  <pose>" << x << " " << y << " " << z << " 0 0 0</pose>\n" // -125.747567
-				<< "  <include>"
-				<< "	<uri>model://Leek</uri>\n"
-				<< "	<static>true</static>\n"
-				<< "  </include>"
-				<< "</model>"
+				<< "  <model name=\"" << modelname << "_" << n << "\">"
+				<< "    <pose>" << x << " " << y << " " << z << " 0 0 0</pose>\n" // -125.747567
+				<< "    <include>"
+				<< "	  <uri>model://" << modelname << "</uri>\n"
+				<< "	  <static>true</static>\n"
+				<< "    </include>"
+				<< "  </model>"
 				<< "</sdf>";
 			parent->InsertModelString(sdftext.str());
 			n++;
